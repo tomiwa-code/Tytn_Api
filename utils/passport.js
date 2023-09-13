544771484617524a666ccfc1d55bea2f8e9e1e92
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2023-09-13 10:36:15 +0100

    Fix: Fixed callback url problem

--- utils/passport.js
+++ utils/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://tytn-api.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       // Check if there's an existing user with the same Google ID
